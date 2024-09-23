c58e3f4af2f8042bb502cf495c912246d2bc3361
kangduu <1365271643@qq.com>
2024-09-23 23:19:41 +0800

    '使用apifox导出swagger的json文件'

--- ant-design-pro-cli-simple/src/services/ant-design-pro/index.ts
+++ ant-design-pro-cli-simple/src/services/ant-design-pro/index.ts
@@ -2,9 +2,9 @@
 /* eslint-disable */
 // API 更新时间：
 // API 唯一标识：
-import * as api from './api';
-import * as login from './login';
+import * as chongwu from './chongwu';
+import * as shilixiangmu from './shilixiangmu';
 export default {
-  api,
-  login,
+  shilixiangmu,
+  chongwu,
 };
---
+++ ant-design-pro-cli-simple/src/services/ant-design-pro/shilixiangmu.ts
@@ -0,0 +1,78 @@
+// @ts-ignore
+/* eslint-disable */
+import { request } from '@umijs/max';
+
+/** 修改宠物信息 PUT /pet */
+export async function putPet(body: {}, options?: { [key: string]: any }) {
+  return request<{ code: number; data: API.Pet }>('/pet', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** 新建宠物信息 POST /pet */
+export async function postPet(
+  body: {
+    /** 宠物名 */
+    name: string;
+    /** 宠物销售状态 */
+    status: string;
+  },
+  options?: { [key: string]: any },
+) {
+  return request<{ code: number; data: API.Pet }>('/pet', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** 查询宠物详情 GET /pet/${param0} */
+export async function getPetId(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getPetIdParams,
+  options?: { [key: string]: any },
+) {
+  const { petId: param0, ...queryParams } = params;
+  return request<{ code: number; data: API.Pet }>(`/pet/${param0}`, {
+    method: 'GET',
+    params: { ...queryParams },
+    ...(options || {}),
+  });
+}
+
+/** 删除宠物信息 DELETE /pet/${param0} */
+export async function deletePetId(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.deletePetIdParams,
+  options?: { [key: string]: any },
+) {
+  const { petId: param0, ...queryParams } = params;
+  return request<{ code: number }>(`/pet/${param0}`, {
+    method: 'DELETE',
+    params: { ...queryParams },
+    ...(options || {}),
+  });
+}
+
+/** 根据状态查找宠物列表 GET /pet/findByStatus */
+export async function getFindByStatus(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getFindByStatusParams,
+  options?: { [key: string]: any },
+) {
+  return request<API.Pet[]>('/pet/findByStatus', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
